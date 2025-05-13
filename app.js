dcdb5ef54a027ad0c2071db1b3382bf63607b6b2
yyy158 <1044733934@qq.com>
2025-05-13 22:15:20 +0800

    first add

---
+++ app.js
@@ -0,0 +1,19 @@
+//导入并使用express框架
+const express = require('express')
+const app = express()
+
+//导入表格工具
+const bodyParser = require('body-parser')
+
+//导入并挂载cors解决跨域
+const cors = require('cors')
+app.use(cors())
+
+//当extended为false时表示值为数组或字符串、true时为任意类型
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+//设置后端接口为3007并进行监听
+app.listen(3007, () => {
+	console.log('hello,this is http:127.0.0.1:3007')
+})
